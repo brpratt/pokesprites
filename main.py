f7dd0f98d65b03c29a86e0374526fb87e1eccee8
Ben Pratt <benjaminrpratt@gmail.com>
2020-06-24 20:42:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
-
 app = FastAPI()
 
 @app.get("/sprites/{id}")
 async def get_sprite(id: int):
-    return FileResponse("images/" + str(id) + ".png")
+    return FileResponse(f"images/{id}.png")
